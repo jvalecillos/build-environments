63c53f8d1fefb5f15ec008911ffa72774b6c4a84
liushuyu <liushuyu011@gmail.com>
2022-10-03 18:30:47 -0600

    linux-mingw: adapt scripts from yuzu ...
    ... now the base image uses Arch Linux

--- linux-mingw/mingw-setup.sh
+++ linux-mingw/mingw-setup.sh
@@ -2,16 +2,8 @@
 # install pefile
 pip3 install pefile
 
-MINGW_URL='https://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/8.1.0/threads-posix/seh/x86_64-8.1.0-release-posix-seh-rt_v6-rev0.7z/download'
-TARGET_DIR='mingw64/x86_64-w64-mingw32/lib/'
-
-echo 'Downloading MinGW replacement binaries...'
-wget -q "${MINGW_URL}" -O 'mingw.7z'
-7z x 'mingw.7z' "${TARGET_DIR}"lib{mf,mfplat,mfuuid}.a
-cp -rv "${TARGET_DIR}"/* '/usr/x86_64-w64-mingw32/lib/'
-
 # SDL2
-SDL2_VER='2.0.16'
+SDL2_VER='2.0.22'
 wget "https://www.libsdl.org/release/SDL2-devel-${SDL2_VER}-mingw.tar.gz"
 tar -zxf "SDL2-devel-${SDL2_VER}-mingw.tar.gz"
 cd SDL2-${SDL2_VER}/
@@ -19,10 +11,10 @@ make install-package arch=x86_64-w64-mingw32 prefix=/usr/x86_64-w64-mingw32;
 cd ..
 
 # ffmpeg
-FFMPEG_VER='4.4'
-FILENAME="ffmpeg-n${FFMPEG_VER}-151-g5e61fce832-win64-gpl-shared-${FFMPEG_VER}"
+FFMPEG_VER='4.4.2'
+FILENAME="ffmpeg-n${FFMPEG_VER}-95-ga8f16d4eb4-win64-gpl-shared-${FFMPEG_VER%.*}"
 echo "Downloading ffmpeg (${FFMPEG_VER})..."
-wget -q -c "https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2021-09-13-12-21/${FILENAME}.zip"
+wget -c "https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2022-10-03-12-38/${FILENAME}.zip"
 7z x "${FILENAME}.zip"
 
 echo "Copying ffmpeg ${FFMPEG_VER} files to sysroot..."
